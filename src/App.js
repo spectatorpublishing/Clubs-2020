c1fdf229d8a88bd8a5400b6721660cbdbaa3c41e
Carl Dobrovic <cadobrovic@gmail.com>
2020-06-27 19:08:00 -0400

    Added styled-components and global styles file.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { MobileAndTablet, Desktop } from 'react-responsive-simple';
 import logo from './logo.svg';
 import './App.css';
 
