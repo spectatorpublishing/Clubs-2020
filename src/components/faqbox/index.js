37fd384f5992ffd124c906951dd4406a8e23ce14
Pazit Schrecker <pazitrose1999@gmail.com>
2020-10-13 17:35:18 -0400

    Created styled components for all parts of FAQBox component

--- src/components/faqbox/index.js
+++ src/components/faqbox/index.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import styled from 'styled-components'
-;
+import React from "react";
+import styled from "styled-components";
 const Box = styled.div`
-    background-color: white;
-    box-shadow: 2px 10px 30px rgba(0, 0, 0, 0.25);
-    border-radius: 7px;
-    text-align: left;
-    width: 20rem;
-    margin: 5rem;
-    padding: 2rem;
+  background-color: white;
+  box-shadow: 2px 10px 30px rgba(0, 0, 0, 0.25);
+  border-radius: 7px;
+  text-align: left;
+  width: 20rem;
+  margin: 5rem;
+  padding: 2rem;
+  font: Roboto;
+  font-style: normal;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const Title = styled.div`
+  font-weight: 500;
+  color: black;
+  padding-bottom: 1rem;
+  align-self: flex-start;
+`;
+
+const Button = styled.div`
+  display: inline;
+  color: #ec6c52;
+  text-align: center;
+  border: 2px solid #ec6c52;
+  border-radius: 5px;
+  padding-top: 0.3rem;
+  padding-bottom: 0.3rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+`;
+
+const BoxText = styled.div`
+  font-style: normal;
+  color: #9a9a9a;
+  align-self: left;
+  padding-bottom: 0.8rem;
 `;
 
 export const FaqBox = ({ title, text, buttonText, buttonLink }) => {
   return (
     <Box>
-        <h3>{title}</h3>
-        <p>{text}</p>
-        <a href={buttonLink}>
-            <button>
-                {buttonText}
-            </button>
-        </a>
+      <Title>{title}</Title>
+      <BoxText>{text}</BoxText>
+      <a href={buttonLink}>
+        <Button>{buttonText}</Button>
+      </a>
     </Box>
-  )
-}
+  );
+};
