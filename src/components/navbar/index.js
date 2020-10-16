2d440c25a411abb22ee913e3b21222e72917a681
fcavuoti <56198661+fcavuoti@users.noreply.github.com>
2020-10-16 21:47:13 +0200

    Created styled components for navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,42 +2,76 @@ import React from 'react';
 import styled from 'styled-components';
 
 const NavWrapper = styled.header`
-  background-color: lightpink;
-  padding: 10px;
-  color: black;
+  position: fixed;
+  background: #48C6DC;
+  padding: 1rem; 
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font: Roboto;
+  font-style: normal;
 
   nav {
     height: 100%;
   }
 `;
 
+const NavHeader = styled.a`
+  margin-left: 3vw;   
+  margin-top: 2vh;
+  text-decoration: none;
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 500;
+  align-self: flex-start;
+`;
+
 const NavLinksContainer = styled.ul`
   display: inline-block;
-  padding: 0;
-  margin-top: 20px;
 
   li {
     display: inline;
-    margin-right: 30px;
+    margin-right: 40px;
+  }
+
+  a {
+    text-decoration: none;
+    color: white;
+    font-size: 1rem;
+    font-weight: 500;
   }
 `;
 
+const Button = styled.div`
+  display: inline;
+  text-align: center;
+  background-color: #EC6C52;
+  border: 2px solid #EC6C52;
+  border-radius: 5px;
+  border-radius: 7px;
+  padding: 0.3rem 1rem;
+`;
+
+
 export const Navbar = () => {
   return (
-    <NavWrapper>
-      <nav>
+    <nav>
+      <NavWrapper> 
+        <NavHeader href="/">Clubs@CU</NavHeader>
         <NavLinksContainer>
           <li>
-            <a href="/">Home</a>
+            <a href="/faq">FAQs</a>
           </li>
           <li>
-            <a href="/explore">Explore Page</a>
+            <a href="/explore">Club Login</a>
           </li>
           <li>
-            <a href="/faq">FAQ Page</a>
-          </li>
+            <Button><a href="/faq">Register Club</a></Button>
+          </li> 
         </NavLinksContainer>
-      </nav>
-    </NavWrapper>
+      </NavWrapper>
+    </nav>
+
   )
 }
