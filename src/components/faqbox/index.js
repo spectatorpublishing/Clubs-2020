6a0251248464d5a10f2010702e71c3fe93cf15e1
Pazit Schrecker <pazitrose1999@gmail.com>
2020-10-18 14:31:12 -0400

    Added mobile compatibility for button

--- src/components/faqbox/index.js
+++ src/components/faqbox/index.js
@@ -2,42 +2,55 @@ import React from "react";
 import styled from "styled-components";
 
 const Box = styled.div`
-  background-color: ${props => props.theme.colors.white};
-  box-shadow: 2px 10px 30px rgba(0, 0, 0, 0.25);
-  border-radius: 7px;
-  text-align: left;
-  width: 20rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  width: 30rem;
+  border-radius: 7px;
+  box-shadow: 2px 10px 30px rgba(0, 0, 0, 0.25);
+  background-color: ${(props) => props.theme.colors.white};
+  padding: 0.5rem 1rem 1rem 1.5rem;
+
+  a {
+    text-decoration: none;
+  }
+
   align-items: flex-end;
 `;
 
-const Title = styled.div`
-  font-weight: 500;
-  color: black;
-  padding-bottom: 1rem;
+const Title = styled.h2`
+  margin-bottom: 0.5rem;
   align-self: flex-start;
 `;
 
-const Button = styled.div`
-  display: inline;
-  color: ${props=>props.theme.colors.red};
-  text-align: center;
-  border: 2px solid;
-  border-color: ${props=>props.theme.colors.red};
-  border-radius: 5px;
-  padding-top: 0.3rem;
-  padding-bottom: 0.3rem;
-  padding-left: 2rem;
-  padding-right: 2rem;
+const BoxWrapper = styled.div`
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    align-content: stretch;
+  }
 `;
 
-const BoxText = styled.div`
-  font-style: normal;
-  color: #9a9a9a;
-  align-self: left;
+const Button = styled.button`
+  color: ${(props) => props.theme.colors.red};
+  border: 2px solid;
+  border-color: ${(props) => props.theme.colors.red};
+  border-radius: 5px;
+  padding: 0.3rem 2rem;
+  font-weight: 600;
+  @media screen and (max-width: 768px) {
+    flex-grow: 2;
+    align-self: center;
+    display: block;
+    width: 100%;
+  }
+`;
+
+const BoxText = styled.p`
+  color: ${(props) => props.theme.colors.gray};
   padding-bottom: 0.8rem;
+  display: block;
+  font-weight: 500;
 `;
 
 export const FaqBox = ({ title, text, buttonText, buttonLink }) => {
@@ -45,13 +58,11 @@ export const FaqBox = ({ title, text, buttonText, buttonLink }) => {
     <Box>
       <Title>{title}</Title>
       <BoxText>{text}</BoxText>
-      <a href={buttonLink}>
-        <Button>{buttonText}</Button>
-      <h3>{title}</h3>
-      <p>{text}</p>
-      <a href={buttonLink}>
-        <button>{buttonText}</button>
-      </a>
+      <BoxWrapper>
+        <a href={buttonLink}>
+          <Button>{buttonText}</Button>
+        </a>
+      </BoxWrapper>
     </Box>
   );
 };
