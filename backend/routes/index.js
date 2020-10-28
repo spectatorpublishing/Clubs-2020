88dc95e16c9f13e50a2aec0d16f486e38b9ec232
Yuanyuting Wang <yw3241@columbia.edu>
2020-10-28 13:43:39 -0400

    connected fe and bd

--- backend/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const clubProfileRoutes = require('./clubProfileRoutes')
-
-router.use("/api/clubProfiles", clubProfileRoutes)
-
-module.exports = router
---
+++ client/build/precache-manifest.7d230392e5f8123f7dd7e38c2cb07cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2535a6cd566d929b98dd407a9147591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b4fdfb1407b11e2360e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e8a9b77b15f875ad9ae1",
+    "url": "/static/js/2.7df93358.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7df93358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4fdfb1407b11e2360e",
+    "url": "/static/js/main.e566ede1.chunk.js"
+  },
+  {
+    "revision": "3360514a47f5be69c255",
+    "url": "/static/js/runtime-main.a796cb08.js"
+  }
+]);
