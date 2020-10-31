fbb7886a6f43655d4e0ad65e177e0e51c2cad674
Yuanyuting Wang <yw3241@columbia.edu>
2020-10-31 19:14:19 -0400

    made var names more descriptive

--- server/routes/clubProfileRoutes.js
+++ server/routes/clubProfileRoutes.js
@@ -1,13 +1,13 @@
-const fns = require('../controllers/clubProfileController')
+const clubProfileControllers = require('../controllers/clubProfileController')
 const router = require('express').Router()
 
-router.route("/filterAndSort").get(fns.filterAndSortBy)
-router.route("/search").get(fns.search)
-router.route("/:id").get(fns.getById)
-router.route("/").get(fns.getAll)
+router.route("/filterAndSort").get(clubProfileControllers.filterAndSortBy)
+router.route("/search").get(clubProfileControllers.search)
+router.route("/:id").get(clubProfileControllers.getById)
+router.route("/").get(clubProfileControllers.getAll)
 
-router.route("/submit").post(fns.create)
-router.route("/update/:id").put(fns.update)
-router.route("/delete/:id").delete(fns.delete)
+router.route("/submit").post(clubProfileControllers.create)
+router.route("/update/:id").put(clubProfileControllers.update)
+router.route("/delete/:id").delete(clubProfileControllers.delete)
 
 module.exports = router
