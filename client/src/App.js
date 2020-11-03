f23a6a0a65d580daa97f0d7e50320316bdaa9147
laurasofi1024 <61126997+laurasofi1024@users.noreply.github.com>
2020-11-03 18:29:46 -0500

    Created Club Profile Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components/navbar';
 import { Explore } from './containers/Explore';
 import { FAQ } from './containers/FAQ';
+import {ClubProfile} from './containers/ClubProfile';
 import { ThemeProvider } from 'styled-components';
 import ProfileCreation from './containers/ProfileCreation'
 import theme from './theme';
@@ -16,6 +17,7 @@ const App = () => {
           <Route path='/explore' component={Explore} />
           <Route path='/faq' component={FAQ} />
           <Route path='/profile-creation' component={ProfileCreation} />
+          <Route path='/club-profile' component={ClubProfile} />
         </Switch>
       </Router>
     </ThemeProvider>
---
+++ client/src/containers/ClubProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ClubProfile = () => {
+    return(
+        <main>
+            <h1>Club Profile</h1>
+        </main>
+    )
+}
