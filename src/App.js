ac8cde262389c213f225877c4ec098e35fb8fcde
N 9 <n9@dyn-160-39-140-196.dyn.columbia.edu>
2020-11-03 19:48:02 -0500

    sign up and confirmation components. can also be used for sign in.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components/navbar';
 import { Explore } from './containers/Explore';
 import { FAQ } from './containers/FAQ';
+import { SignUp } from './containers/SignUp';
+import { Confirmation } from './containers/Confirmation';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 
@@ -14,6 +16,8 @@ const App = () => {
         <Switch>
           <Route path='/explore' component={Explore} />
           <Route path='/faq' component={FAQ} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/confirm' component={Confirmation} />
         </Switch>
       </Router>
     </ThemeProvider>
