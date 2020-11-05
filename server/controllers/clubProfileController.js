ca43519bb18ed85e039a45355971b49e987fba17
BuildTools <unconfigured@null.spigotmc.org>
2020-11-04 19:22:36 -0500

    Update clubProfileController.js

--- server/controllers/clubProfileController.js
+++ server/controllers/clubProfileController.js
@@ -1,6 +1,3 @@
-const clubOverview = require("../models/ClubOverviewModel")
-const clubProfile = require("../models/ClubProfileModel")
-
 module.exports = {
     getAll: function(req, res) {
         // TODO
@@ -9,30 +6,15 @@ module.exports = {
     },
     getById: function(req, res) {
         // TODO; req.params.id
-        clubProfile.findById(req.params.id)
-                .then(clubprofile => res.json(clubprofile))
-                .catch(err => res.status(422).json(err));
-
     },
     create: function(req, res) {
         // TODO; req.body contains profile information
-        clubProfile.create(req.body)
-                .then(newclubProfile => res.json(newclubProfile))
-                .catch(err => res.status(422).json(err))
     },
     update: function(req, res) {
         // TODO; req.body contains profile information and req.params.id
-        clubProfile.findOneAndUpdate({ _id: req.params.id}, req.body)
-                .then(clubprofile => res.json(clubprofile))
-                .catch(err => res.status(422).json(err))
     },
     delete: function(req, res) {
         // TODO: req.params.id
-        clubProfile.findById({ _id: req.params.id })
-                .then(clubprofile => clubprofile.remove())
-                .then(allprofiles => res.json(allprofiles))
-                .catch(err => res.status(422).json(err))
-                
     },
     filterAndSortBy: function(req, res) {
         // TODO; req.query contains filter and/or sort information
