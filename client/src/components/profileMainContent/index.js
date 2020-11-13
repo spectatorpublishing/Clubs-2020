e886a25554d66942934f4bdb8364806509e1deba
laurasofi1024 <61126997+laurasofi1024@users.noreply.github.com>
2020-11-13 15:45:52 -0500

    Update index.js

--- client/src/components/profileMainContent/index.js
+++ client/src/components/profileMainContent/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
 import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
-import { faEdit } from "@fortawesome/free-regular-svg-icons";
 
-export const ProfileMain = ({name, description, lastUpdated, acceptingMembers, applicationRequired, highlights, howToJoin, applicationLink}) => {
-    const Members = acceptingMembers ? <><FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon><p>Open</p></> : <><FontAwesomeIcon icon={faTimesCircle}></FontAwesomeIcon><p>Closed</p></>;
-    const Application = applicationRequired ? <p>Application</p> : <p>No Application</p>;
-        
+export const ProfileMain = ({name, description, lastUpdated, highlights, howToJoin, applicationLink}) => {   
     return (
         <Container>
             <h1>{name}</h1>
@@ -20,7 +14,7 @@ export const ProfileMain = ({name, description, lastUpdated, acceptingMembers, a
             <p2>{highlights.map(highlight => <Highlight text={highlight}/>)}</p2>
             <h2>How to Join</h2>
             <p2>{howToJoin}</p2>
-            <div><a href={applicationLink}><Button>Apply Here</Button></a></div>
+            <div><Button href={applicationLink}>Apply Here</Button></div>
         </Container>
     );
 };
@@ -28,17 +22,16 @@ export const ProfileMain = ({name, description, lastUpdated, acceptingMembers, a
 export default ProfileMain;
 
 const Highlight = ({text}) => {
-
     return (
         <Row>
             <FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon>{text}
         </Row>
     )
-
 };
 
 const Row = styled.div`
     display: in-line;
+    padding: 5px;
     svg{
         padding-right: 10px;
         color: #42B7CB;
@@ -46,8 +39,10 @@ const Row = styled.div`
 `;
 
 const Container = styled.div`
+    
     p,p1,p2{
         color: #696969;
+        line-height: 1.5em;
     }
     
     p1{
@@ -57,13 +52,11 @@ const Container = styled.div`
     h1{
         margin-bottom: 0px;
     }
-
-    a{
-        color: #EC6C52;
-    }
 `;
 
-const Button = styled.div`
+const Button = styled.a`
+    color: #EC6C52;
+    text-decoration: none;
     border: 2px solid #EC6C52;
     display: inline-block;
     padding: 10px;
