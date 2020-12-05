7c0ffb3272511cbfe8c65f92b1918ea1387168db
Yu-Chen Huang <yuchenxhuang@gmail.com>
2020-12-05 18:33:46 -0500

    skeleton

---
+++ client/src/components/accountTag/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export const AccountTag = () => {
+    return(
+      <BoxWrapper>
+
+
+
+      </BoxWrapper>
+    );
+};
+
+
+
+
+const BoxWrapper = styled.div`
+   
+    height: 100px;
+    width: 100px;
+    background-color: blue;
+
+
+`;
---
+++ client/src/containers/AccountTagTest.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import { AccountTag } from "../components/accountTag/index";
+
+
+export const AccountTagTest = () => {
+    return(
+      <>
+          <AccountTag></AccountTag>
+      </>
+    );
+};
