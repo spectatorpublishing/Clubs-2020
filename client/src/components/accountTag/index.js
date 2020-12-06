f9d490df55f718685094dd52c66810d7ac4a196d
Yu-Chen Huang <yuchenxhuang@gmail.com>
2020-12-06 01:00:07 -0500

    Update index.js

--- client/src/components/accountTag/index.js
+++ client/src/components/accountTag/index.js
@@ -5,9 +5,8 @@ import styled from "styled-components";
 export const AccountTag = () => {
     return(
       <BoxWrapper>
-
-
-
+          <Emoji>🎉</Emoji>
+          <Text>Thank you for creating an account! Here is a preview of what other students can see. Hit edit anytime to change your information.</Text>
       </BoxWrapper>
     );
 };
@@ -16,10 +15,26 @@ export const AccountTag = () => {
 
 
 const BoxWrapper = styled.div`
-   
-    height: 100px;
-    width: 100px;
-    background-color: blue;
+    margin: 20px;
+    padding: 10px;
+    width: auto;
+    background-color: white;
+    border-radius: 10px;
+    display: flex;
+    box-shadow: 0px 0px 15px 0px #C1C1C1;
+    align-items: center;
+`;
+
+const Emoji = styled.div`
+    margin: 10px 10px 10px 20px;
+    font-size: 30px;
+`;
+
+const Text = styled.div`
+    color: black;
+    margin: 10px;
+    font-weight: 600;
+
 
 
 `;
