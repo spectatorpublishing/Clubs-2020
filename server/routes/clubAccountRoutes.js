6fdbf8e121100fbf00ecc05eef76e3d68e7bb77b
Yuanyuting Wang <yw3241@columbia.edu>
2020-12-05 21:35:22 -0500

    configured endpoints and controller functions

---
+++ server/routes/clubAccountRoutes.js
@@ -0,0 +1,14 @@
+const clubAccountControllers = require("../controllers/clubAccountControllers")
+const router = require('express').Router()
+
+router.route("/create/:firebaseId").post(clubAccountControllers.create)
+router.route("/update/:id").put(clubAccountControllers.changeVerificationStatus)
+router.route("/delete/:firebaseId").delete(clubAccountControllers.delete)
+
+router.route("/getProfile/:id").get(clubAccountControllers.getProfile)
+router.route("/getById/:id").get(clubAccountControllers.getById)
+router.route("/getByFirebaseId/:firebaseId").get(clubAccountControllers.getByFirebaseId)
+
+router.route("/getAll").get(clubAccountControllers.getAll)
+
+module.exports = router
