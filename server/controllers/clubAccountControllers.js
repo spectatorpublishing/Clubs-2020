56749a31dd026ae5fadcb7458aefde83797a246b
Tanner Treasure <tt2717@columbia.edu>
2020-12-09 20:18:49 -0500

    Update clubAccountControllers.js

--- server/controllers/clubAccountControllers.js
+++ server/controllers/clubAccountControllers.js
@@ -2,7 +2,6 @@ const clubAccount = require("../models/ClubAccountModel")
 const clubProfile = require("../models/ClubProfileModel")
 const config = require("../config")
 
-
 module.exports = {
     create: function(req, res){
         // TODO
@@ -38,23 +37,23 @@ module.exports = {
             .sort( {creationDate: 1, lastUpdateDate: -1} ).then(
                 q1 =>{ 
                     rdata.pending = JSON.parse(JSON.stringify(q1))
-                    clubProfile.find({
+                    clubAccount.find({
                         verificationStatus: 'accepted',
                         lastUpdateDate:  {
                             //Updated within the last 14 days
                             $gte: date
                         }
-                    }).sort( {lastUpdateDate: 1, creationDate: 1} ).then(
+                    }).sort( {lastUpdateDate: -1} ).then(
                         q2 => {
                             rdata.accepted = JSON.parse(JSON.stringify(q2))
 
-                            clubProfile.find({
+                            clubAccount.find({
                                 verificationStatus: 'denied',
                                 lastUpdateDate:  {
                                     //Updated within the last 14 days
                                     $gte: date
                                 }
-                            }).sort( {lastUpdateDate: -1, creationDate: -1} ).then(
+                            }).sort( {lastUpdateDate: -1} ).then(
                                 q3 => {
                                     rdata.denied = JSON.parse(JSON.stringify(q3))
                                     res.json(rdata)
