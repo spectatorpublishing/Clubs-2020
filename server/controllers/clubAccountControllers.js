43780560af7f15698c39eb9b5dfa425ba55d0812
Tanner Treasure <tt2717@columbia.edu>
2020-12-09 19:07:27 -0500

    Addressed comments
    changed config and separated queries into 3 different sections of json object

--- server/controllers/clubAccountControllers.js
+++ server/controllers/clubAccountControllers.js
@@ -1,5 +1,7 @@
 const clubAccount = require("../models/ClubAccountModel")
 const clubProfile = require("../models/ClubProfileModel")
+const config = require(".../config/index")
+
 
 module.exports = {
     create: function(req, res){
@@ -28,23 +30,37 @@ module.exports = {
     },
     getAll: function(req, res){
         //query for pending and sort, query for nonpending and sort
-        var q1 = clubAccount.find( {verificationStatus: 'pending'} )
-            .sort( {creationDate: -1, lastUpdateDate: -1} );
-        var q2 = clubProfile.find({
-            verificationStatus: {$in: ['accepted', 'denied']},
-            lastUpdateDate:  {
-                //Updated within the last 14 days
-                $gte: (new Date(ISODate().getTime() - 1000 * 3600 * 24 * 14))
-            }
-        }).sort( {lastUpdateDate: -1, creationDate: -1} );
-        
-        //combine pending and nonpending then return full array if no issues
-        var pendingData = JSON.parse(JSON.stringify(q1));
-        var nonPendingData = JSON.parse(JSON.stringify(q2));
+        var rdata = {};
+        clubAccount.find( {verificationStatus: 'pending'} )
+            .sort( {creationDate: -1, lastUpdateDate: -1} ).then(
+                q1 =>{ 
+                    rdata.pending = q1
+                    clubProfile.find({
+                        verificationStatus: 'accepted',
+                        lastUpdateDate:  {
+                            //Updated within the last 14 days
+                            $gte: (new Date(ISODate().getTime() - 1000 * 3600 * 24 * config.timeFrame))
+                        }
+                    }).sort( {lastUpdateDate: -1, creationDate: -1} ).then(
+                        q2 => {
+                            rdata.accepted = q2
+                            clubProfile.find({
+                                verificationStatus: 'denied',
+                                lastUpdateDate:  {
+                                    //Updated within the last 14 days
+                                    $gte: (new Date(ISODate().getTime() - 1000 * 3600 * 24 * config.timeFrame))
+                                }
+                            }).sort( {lastUpdateDate: -1, creationDate: -1} ).then(
+                                q3 => {
+                                    rdata.denied = q3
+                                    res.json(rdata)
+                                }
+                            ).catch(err => res.status(422).json(err));
 
-        pendingData.concat(nonPendingData)
-            .then(rdata => res.json(rdata))
-            .catch(err => res.status(422).json(err))
+                        }
+                    ).catch(err => res.status(422).json(err));
 
+                }
+            ).catch(err => res.status(422).json(err));
     }
 }
