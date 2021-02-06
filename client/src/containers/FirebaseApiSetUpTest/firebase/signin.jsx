7c9ebeeb7d9e17a581048a99549d9d841b2ea99c
Cremebrutale <caroline.hoang@columbiaspectator.com>
2021-02-05 21:32:25 -0500

    rearraged repo structure

--- client/src/containers/FirebaseApiSetUpTest/firebase/signin.jsx
+++ client/src/containers/FirebaseApiSetUpTest/firebase/signin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import * as firebase from '../../firebase';
+import * as firebase from '../../../UserAuthUtilities/firebase';
 
 const Background = styled.div`
   background-color: ${props => props.theme.colors.lightGray};
