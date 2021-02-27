e466d8acc216056df858986cdae089f4f97d938b
Matthew <mdw2166@columbia.edu>
2021-02-27 18:04:08 -0500

    Saved and commited the adCarrier index.js file as well, without redundant styling..

--- client/src/components/adCarrier/index.js
+++ client/src/components/adCarrier/index.js
@@ -3,23 +3,12 @@ import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 import styled from 'styled-components';
 
 
-const Style = styled.div`
-  background-color: red;
-  color: black;
-  height: ${props => props.Height}px;
-  width: ${props => props.Width}px;
-  margin-left: auto;
-  margin-right: auto;
-`
-
 const adCarrier = ({Height, Width, Path}) =>{
   return (
     <div>
-    <h1>PlaceholderText</h1>
-    <Style>
       <DFPSlotsProvider 
         dfpNetworkId="59699124"
-        /* sizeMapping={[{viewport:[sizeMobile,sizeMobile], sizes: []}, //Alternate units?
+        /* sizeMapping={[{viewport:[sizeMobile,sizeMobile], sizes: []},
                       {viewport:[sizeTab,sizeTab], sizes: []},
                       {viewport:[sizeCPU,sizeCPU], sizes: []} ]} //ADSLOT ACCEPTS, DEPENDENT USAGE
         //collapseEmptyDivs Turn back on for final product */
@@ -28,54 +17,26 @@ const adCarrier = ({Height, Width, Path}) =>{
           <AdSlot sizes={[[Width, Height]]} adUnit={Path} />
         </div>
       </DFPSlotsProvider>
-    </Style> 
-    </div>
+    </div> 
   );
 }
 
 export default adCarrier;
 
 
-
-
-
-
-// "---------------"
-
-/* 
-export default class adUnit extends Component {
-  render() {
-    return (
-      <DFPSlotsProvider dfpNetworkId="needToGet">
-        <div>
-          <AdSlot sizes={[[728, 90], [300, 250]]} adUnit="?{props.path}?" />
-        </div>
-      </DFPSlotsProvider>
-    );
-  }
-}
- */
-
-// "---------------"
-
-/* Need to determine:
- - What info is passed in / what props am I getting to call and their props
- - dfpNetworkId (example offered 59699124), similarly adUnitPath (example used props.path)
- - "fluid" sizes? Dont anticipate needing this, should render based off of prop...
- - Will sizing accept alterate units?
  
-
+/*
 NOTE: ALL REQUIRED AD SLOTS BASED ON PROVIDED MOCKUPS:
- - Home:
+ - Explore:
    - DESKTOP Top banner, 728x90
    - MOBILE Top banner, 328x50
  - Club profile:
    - UNIVERSAL Sidebar, 300x250
 
 
-Check necessity later:
- - adSenseAttributes
- - Rendering sequence and desires - see last 5 AdSlot props in documentation
- - Refreshing ads on certain intervals, money based? See DFPManager docs, 'refresh'
- - Disable ad personalization? Money based?
+Check necessity later, when ads resources are given to actually link content:
+ - adSenseAttributes.
+ - Rendering sequence and desires - see last 5 AdSlot props in documentation.
+ - Refreshing ads on certain intervals, which may affect revenue. See DFPManager docs, 'refresh'
+ - Option to disable ad personalization.
  */
